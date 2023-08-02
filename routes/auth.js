cfe49c9d420798a6035c1c8c970e108c8c493407
danipreciado <danielapca96@gmail.com>
2023-08-01 17:41:22 -0700

    created function that returns token if user email and pass match db data

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-console */
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const config = require('../config');
+const User = require('../models/User');
 
 const { secret } = config;
 
@@ -17,19 +20,39 @@ module.exports = (app, nextMain) => {
    * @code {400} si no se proveen `email` o `password` o ninguno de los dos
    * @auth No requiere autenticación
    */
-  app.post('/auth', (req, resp, next) => {
+  app.post('/auth', async (req, resp, next) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
       return next(400);
     }
 
+    const userExists = await User.findOne({ email });
+
+    if (!userExists) {
+      return next(401);
+    }
+
+    const passwordMatches = await bcrypt.compare(password, userExists.password);
+    const { role, _id } = userExists;
+    if (!passwordMatches) {
+      return next(401);
+    }
     // TODO: autenticar a la usuarix
     // Hay que confirmar si el email y password
     // coinciden con un user en la base de datos
     // Si coinciden, manda un access token creado con jwt
+    // Autenticación exitosa: Crear un token de acceso con jwt
+    const token = jwt.sign({
+      _id,
+      email,
+      password,
+      role,
+    }, secret, { expiresIn: '1h' });
+    // Enviar el token de acceso como respuesta en el cuerpo de la respuesta JSON
+    return resp.json({ token });
 
-    next();
+    /*  next(); */
   });
 
   return nextMain();
